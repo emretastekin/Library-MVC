3fa8e1dec9e86a570889c93f381fc9d7e79e561e
Emre Taştekin <emretastekin2369@gmail.com>
2024-07-11 11:46:38 +0300

    .

--- LibraryMVC/LibraryMVC/Models/Author.cs
+++ LibraryMVC/LibraryMVC/Models/Author.cs
@@ -4,7 +4,7 @@ namespace LibraryMVC.Models
 	public class Author
 	{
         public long Id { get; set; }
-        public string FullName { get; set; } = "";  //NULL olamaz
+        public string FullName { get; set; } = "";  //NULL olamaz.
         public string? Biography { get; set; }
         public short BirthDate { get; set; }
         public short? DeathDate { get; set; }  //DeadTime Null olabilir
